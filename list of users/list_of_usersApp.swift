e02ee624167364eb392cf98e6a3e880b5a6b6c7f
kalayn <kalyan>
2026-03-13 14:41:11 -0500

    Initial Commit

---
+++ list of users/list_of_usersApp.swift
@@ -0,0 +1,17 @@
+//
+//  list_of_usersApp.swift
+//  list of users
+//
+//  Created by kalyan on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct list_of_usersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
